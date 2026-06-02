bbe52b5f1176902daa3b9e140e85b276e8d2adc8
bukatee4056 <ebuka40402@gmail.com>
2026-06-02 10:12:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 window.onload = function () {
-  alert("APP JS LOADED");
 
   const button = document.querySelector("button");
+  const input = document.querySelector("input");
+  const result = document.getElementById("result");
 
   button.onclick = function () {
-    alert("BUTTON CLICKED");
+
+    const value = input.value.trim();
+
+    if (!value) {
+      alert("Enter tracking number");
+      return;
+    }
+
+    result.innerHTML = `
+      <h3>Tracking Number: ${value}</h3>
+      <p>Status system is now working</p>
+    `;
   };
+
 };
