4bdc5ed8838145c8159543a575c53e91abbf02d0
bukatee4056 <ebuka40402@gmail.com>
2026-06-02 03:14:43 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
-console.log("Firebase file ready");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwjcKxnViESMXyCzhML2sZPbA_HBzytMg",
+  authDomain: "diplomatic-courier-service.firebaseapp.com",
+  projectId: "diplomatic-courier-service",
+  storageBucket: "diplomatic-courier-service.firebasestorage.app",
+  messagingSenderId: "601909831194",
+  appId: "1:601909831194:web:d1d2edf11f4aa5871f53a3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected");
